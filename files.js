2ad1864cf9f7885d2962d0019076c9611e49dc70
Obvious Plays <111094267+ObviousTeam@users.noreply.github.com>
2022-11-15 08:16:38 -0500

    Create files.js

---
+++ files.js
@@ -0,0 +1,14 @@
+document.getElementById("vex3").src
+	= "https://www.obviousplays.tk/gfiles/vex3";
+
+document.getElementById("vex4").src
+	= "https://www.obviousplays.tk/gfiles/vex4";
+	
+document.getElementById("vex5").src
+	= "https://www.obviousplays.tk/gfiles/vex5";
+	
+document.getElementById("vex6").src
+	= "https://www.obviousplays.tk/Gfiles6/vex6";
+	
+document.getElementById("slope").src
+	= "https://www.obviousplays.tk/gfiles/slope";
